26086a5d909e59127118473b9e8dc6111bb63d6c
EdgarRB <edgarerb@hotmail.es>
2023-09-25 17:01:55 +0200

    todo listo. queda poner los proyectos y hacerlo responsive

--- data.js
+++ data.js
@@ -1,58 +1,112 @@
 const proyectos = [
-    {
-        titulo: 'Pokédex',
-        descripcion: 'En este proyecto, se trabaja con PokeAPI, una API con todos los datos de los pokémon, intentando emular a una Pokédex, en Angular.',
-        link: 'https://pokedex-by-edgar.netlify.app/',
-        imagen: 'Pokedex.png',
-        tecnologias: ['HTML', 'SCSS', 'JS','TS','Angular','API','GitHub','Netlify']
-    },
-    {
-        titulo: 'Proyecto 2',
-        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-        link: 'https://www.google.com',
-        imagen: 'placeholder.png',
-        tecnologias: ['HTML', 'CSS', 'JS']
-    },
-    {
-        titulo: 'Proyecto 3',
-        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-        link: 'https://www.google.com',
-        imagen: 'placeholder.png',
-        tecnologias: ['HTML', 'CSS', 'JS']
-    },
-    {
-        titulo: 'Proyecto 4',
-        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-        link: 'https://www.google.com',
-        imagen: 'placeholder.png',
-        tecnologias: ['HTML', 'CSS', 'JS']
-    },
-    {
-        titulo: 'Proyecto 5',
-        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-        link: 'https://www.google.com',
-        imagen: 'placeholder.png',
-        tecnologias: ['HTML', 'CSS', 'JS']
-    },
-    {
-        titulo: 'Proyecto 6',
-        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-        link: 'https://www.google.com',
-        imagen: 'placeholder.png',
-        tecnologias: ['HTML', 'CSS', 'JS']
-    },
-    {
-        titulo: 'Proyecto 7',
-        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-        link: 'https://www.google.com',
-        imagen: 'placeholder.png',
-        tecnologias: ['HTML', 'CSS', 'JS']
-    },
-    {
-        titulo: 'Proyecto 8',
-        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-        link: 'https://www.google.com',
-        imagen: 'placeholder.png',
-        tecnologias: ['HTML', 'CSS', 'JS']
-    }
-]
+  {
+    titulo: "Pokédex",
+    descripcion:
+      "En este proyecto, se trabaja con PokeAPI, una API con todos los datos de los pokémon, intentando emular a una Pokédex, en Angular.",
+    link: "https://pokedex-by-edgar.netlify.app/",
+    imagen: "Pokedex.png",
+    tecnologias: [
+      "HTML",
+      "SCSS",
+      "JS",
+      "TS",
+      "Angular",
+      "API",
+      "GitHub",
+      "Netlify",
+    ],
+  },
+  {
+    titulo: "Proyecto 2",
+    descripcion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    link: "https://www.google.com",
+    imagen: "placeholder.png",
+    tecnologias: ["HTML", "CSS", "JS"],
+  },
+  {
+    titulo: "Proyecto 3",
+    descripcion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    link: "https://www.google.com",
+    imagen: "placeholder.png",
+    tecnologias: ["HTML", "CSS", "JS"],
+  },
+  {
+    titulo: "Proyecto 4",
+    descripcion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    link: "https://www.google.com",
+    imagen: "placeholder.png",
+    tecnologias: ["HTML", "CSS", "JS"],
+  },
+  {
+    titulo: "Proyecto 5",
+    descripcion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    link: "https://www.google.com",
+    imagen: "placeholder.png",
+    tecnologias: ["HTML", "CSS", "JS"],
+  },
+  {
+    titulo: "Proyecto 6",
+    descripcion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    link: "https://www.google.com",
+    imagen: "placeholder.png",
+    tecnologias: ["HTML", "CSS", "JS"],
+  },
+  {
+    titulo: "Proyecto 7",
+    descripcion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    link: "https://www.google.com",
+    imagen: "placeholder.png",
+    tecnologias: ["HTML", "CSS", "JS"],
+  },
+  {
+    titulo: "Proyecto 8",
+    descripcion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    link: "https://www.google.com",
+    imagen: "placeholder.png",
+    tecnologias: ["HTML", "CSS", "JS"],
+  },
+];
+
+const informacionPersonal = {
+  nombre: "Edgar",
+  subtitulo: "Desarrollador Web",
+  imagen: "./img/Edgar.jpg",
+  otros: [
+    ["Nacionalidad", "Español"],
+    [
+      "Edad",
+      new Date(new Date() - new Date("1991-01-14")).getFullYear() - 1970
+    ],
+  ],
+  idiomas: [
+    ["Español", "Nativo"],
+    ["Inglés", "Bueno"],
+    ["Catalán", "Nativo"],
+  ],
+  tecnologias: [
+    ["HTML", 8],
+    ["CSS", 7],
+    ["JavaScript", 7],
+    ["TypeScript", 6],
+    ["Angular", 5],
+    ["Node", 7],
+    ["JQuery", 7],
+    ["React", 5],
+    ["SCSS", 7],
+    ["BootStrap", 8],
+    ["MySQL", 5],
+    ["MongoDB", 6],
+    ["PHP", 5],
+  ],
+  redes: [
+    ["GitHub", "https://github.com/shadoblack/"],
+    ["LinkedIn", "https://www.linkedin.com/in/edrobl91/"]
+  ],
+};
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const proyectosContainer = document.getElementById("tarjetasContainer");
+const aside = document.getElementsByTagName("aside")[0];
 const getTecnologias = (tecnologias) => {
   let res = "";
   tecnologias.forEach((tecnologia) => {
@@ -6,6 +7,8 @@ const getTecnologias = (tecnologias) => {
   });
   return res;
 };
+
+//auto creacion proyectos
 
 proyectos.forEach((proyecto) => {
   const nuevoProyecto = document.createElement("div");
@@ -20,3 +23,59 @@ proyectos.forEach((proyecto) => {
   </div>
   <a href="${proyecto.link}" target="_blank">Ver proyecto</a>`;
 });
+
+//creacion informacion personal
+const nuevaPresentacion = document.createElement("div");
+nuevaPresentacion.classList = "presentacion";
+nuevaPresentacion.innerHTML = `<img src="${informacionPersonal.imagen}" alt="foto de perfil">`;
+informacionPersonal.otros.forEach((dato) => {
+  nuevaPresentacion.innerHTML += `<div>
+  <span>${dato[0]}:</span>
+  <span>${dato[1]}</span>
+  </div>`;
+});
+aside.appendChild(nuevaPresentacion);
+
+//idiomas
+const nuevoIdiomas = document.createElement("div");
+nuevoIdiomas.classList = "idioma";
+informacionPersonal.idiomas.forEach((dato) => {
+  nuevoIdiomas.innerHTML += `<div>
+  <span>${dato[0]}:</span>
+  <span>${dato[1]}</span>
+  </div>`;
+});
+aside.appendChild(nuevoIdiomas);
+//lenguajes de programacion
+const nuevoLenguajesProgramacion = document.createElement("div");
+nuevoLenguajesProgramacion.classList = "idioma";
+informacionPersonal.tecnologias.forEach((dato) => {
+  nuevoLenguajesProgramacion.innerHTML += `<div>
+  <span>${dato[0]}</span>
+  <span>${dato[1]}</span>
+  </div>
+  <progress max ="10" value="${dato[1]}"></progress>`;
+});
+aside.appendChild(nuevoLenguajesProgramacion);
+//links
+
+const getIconoRed = (red) => {
+  switch (red) {
+    case "GitHub":
+      return "github.svg";
+    case "LinkedIn":
+      return "linkedin.svg";
+    default:
+      return "";
+  }
+};
+
+const nuevoRedes = document.createElement("div");
+nuevoRedes.classList = "redes";
+informacionPersonal.redes.forEach(dato => {
+  nuevoRedes.innerHTML += `<a href="${dato[1]} target="_blank">
+  <img src="img/${getIconoRed(dato[0])}">
+  </a>
+  `
+});
+aside.appendChild(nuevoRedes);
